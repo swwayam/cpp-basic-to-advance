f9031ba461742680dff423da03bce0a2acf6c8f7
swwayam <codingswayam24@gmail.com>
2023-08-18 21:02:55 +0530

    basics cpp

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+// Assignment Operators
+// = , += , -=, *=, /=
+
+// Logical Operators
+// and -> && , or -> || , not -> !
+
+// Relational Operators or Comparasion Operators
+// < , > , <= , >= , <=> 
+
+
+// Arithmetic Operators
+// + , - , / , * , %
+
+// Ternary or Conditional Operators
+// condition ? case1 : case2
+
---
+++ variables_data_type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// using namespace std;
+int main(){
+    char g = 'A'; // character data type -> 1 byte
+    int a = 20; // integer data type -> 4 bytes
+    float b = 29.10; // float data type -> 4 byte
+    double c = 11.11; // double data type -> 8 byte
+    bool f = true; // boolean data type -> 1 byte
+    // void empty -> 0 Byte
+    std::string name = "swayam"; // string data type for text 
+    std::cout<<"Hello World";
+    return 0;
+}
