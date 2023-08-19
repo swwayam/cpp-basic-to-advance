96846af080fed46cb210a24f382495c6416203fa
swwayam <codingswayam24@gmail.com>
2023-08-19 10:11:28 +0530

    Done with c++ string functions

---
+++ string_and_methods.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main()
+{
+
+    std::string name = "John .C. Doe";
+    std::string name1 = "Magnus .A. Carlson";
+    // Length method
+    std::cout << name.length() << '\n';
+
+    // Swap method
+    std::swap(name, name1);
+    std::cout << name << " second name " << name1 << '\n';
+
+    // Size method
+    std::cout << size(name) << '\n';
+
+    // resize method ->  If num > size() then, the rest of characters are initialized by the ‘\0’.
+    name.resize(20);
+    std::cout << name << '\n';
+
+    // find method -> second param is optional denotes the starting point | not found return grabage value
+    std::cout << name.find("a", 9) << '\n';
+
+    // push_back -> push passed char to the end of string
+    name.push_back('c');
+    std::cout << name << '\n';
+
+    // pop_back -> removes the last char from the string
+    name1.pop_back();
+    std::cout << name1 << '\n';
+
+    // clear -> removes all the elements
+    // name1.clear();
+    // std::cout << name1 << '\n';
+
+    // compare() ->
+    // It returns 0 if str1 is equal to str2. It returns an integer value less than zero if str1 is less than str2.It returns an integer value greater than zero if str1 is greater than str2.
+    std::string str = "aqua man";
+    std::string str1 = "aqua mann";
+    std::cout << str.compare(str1) << '\n';
+
+
+    // erase() -> used to remove certain part of string
+    std::cout<<name.erase(7)<<'\n';
+    std::cout<<name<<'\n';
+
+    
+
+    return 0;
+}
