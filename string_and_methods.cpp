a0a5e9381bbbcbc75108ef3da0d905739ffdb3ac
swwayam <codingswayam24@gmail.com>
2023-08-19 10:59:55 +0530

    Completed various cpp string methods

--- string_and_methods.cpp
+++ string_and_methods.cpp
@@ -3,6 +3,8 @@
 int main()
 {
 
+    // CPP String Methods
+
     std::string name = "John .C. Doe";
     std::string name1 = "Magnus .A. Carlson";
     // Length method
@@ -42,10 +44,40 @@ int main()
 
 
     // erase() -> used to remove certain part of string
-    std::cout<<name.erase(7)<<'\n';
-    std::cout<<name<<'\n';
+    // std::cout<<name.erase(7)<<'\n';
+    // std::cout<<name<<'\n';
 
-    
+    // CPP string iterator functions
+
+    // str.begin() -> needs string::iterator to store the iteration
+    std::string::iterator it;
+    it = name.begin();
+    std::cout<<*it<<'\n';
+
+    // str.end() -> needs string::iterator to store the iteration
+    std::string::iterator it1;
+    it1 = name.end() - 1; // the last occurance is empty -1 to get the last character 
+    std::cout<<*it1<<'\n';
+
+    // rfind() -> finding from last occurances
+    std::cout<<name.rfind('a')<<'\n';
+
+    // rebegin() -> returns the last character starts the iterator from the last 
+    // rend() -> returns the first character
+    std::cout<<*name.rbegin()<<'\n';
+  
+
+    // String capactiy functions - length , capacity , resize , shrink_to_fit
+
+    // capacity -> returns the capacity assigned by the compiler
+    std::cout<<name.capacity()<<'\n';
+
+    // shrink_to_fit -> decreases the capacity of the string and makes it equal to the minimum
+    name.shrink_to_fit();
+    std::cout<<name.capacity()<<'\n';
+
+    std::cout<<name<<'\n';
+    std::cout<<name1<<'\n';
 
     return 0;
 }
